7030497582cfe3ced1eb834d1145eb12436bffcb
Leon Junior <lmj@icomp.ufam.edu.br>
2020-02-18 14:10:30 -0400

    anime detail collapse toolbar

--- app/src/main/java/com/manickchand/androidanimelist/ui/animeDetails/AnimeDetailActivity.kt
+++ app/src/main/java/com/manickchand/androidanimelist/ui/animeDetails/AnimeDetailActivity.kt
@@ -2,22 +2,18 @@ package com.manickchand.androidanimelist.ui.animeDetails
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.material.snackbar.Snackbar
 import com.manickchand.androidanimelist.R
 import com.manickchand.androidanimelist.models.AnimeDetail
-import com.manickchand.androidanimelist.util.TAG_DEBUC
 import com.manickchand.androidanimelist.util.loadImageView
 import com.manickchand.androidanimelist.util.videoUrlToThumbUrl
 import kotlinx.android.synthetic.main.activity_anime_detail.*
-import kotlinx.android.synthetic.main.content_anime_detail.*
+
 
 class AnimeDetailActivity : AppCompatActivity() {
 
@@ -26,6 +22,13 @@ class AnimeDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anime_detail)
+        setSupportActionBar(toolbar)
+        title = ""
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar!!.setDisplayShowTitleEnabled(false)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.setFlags(
@@ -41,20 +44,19 @@ class AnimeDetailActivity : AppCompatActivity() {
             setData(it)
         })
 
-        iv_back.setOnClickListener{
-            finish()
-        }
-
         animeDetailViewModel.getAnime(animeId)
 
     }
 
     private fun setData(anime: AnimeDetail?) {
+
         val url = videoUrlToThumbUrl(anime!!.trailer_url ?: "")
         loadImageView(iv_anime_video_thumb, url)
+        loadImageView(iv_anime_img_detail, anime.image_url)
 
         tv_anime_detail_score.text = anime.score.toString()
-        tv_anime_title_detail.text = anime.title
+        //tv_anime_title_detail.text = anime.title
+        title = anime.title
         tv_anime_detail_description.text = anime.synopsis
     }
 
