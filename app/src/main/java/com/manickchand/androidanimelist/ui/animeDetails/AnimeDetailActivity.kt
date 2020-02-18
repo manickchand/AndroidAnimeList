b7c72dd0c3f93eb05ed5d672c43cd1783d8033ea
Leon <lmj@icomp.ufam.edu.br>
2020-02-17 22:08:22 -0400

    (wip) create layout anime details

--- app/src/main/java/com/manickchand/androidanimelist/ui/animeDetails/AnimeDetailActivity.kt
+++ app/src/main/java/com/manickchand/androidanimelist/ui/animeDetails/AnimeDetailActivity.kt
@@ -3,8 +3,10 @@ package com.manickchand.androidanimelist.ui.animeDetails
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -15,32 +17,45 @@ import com.manickchand.androidanimelist.util.TAG_DEBUC
 import com.manickchand.androidanimelist.util.loadImageView
 import com.manickchand.androidanimelist.util.videoUrlToThumbUrl
 import kotlinx.android.synthetic.main.activity_anime_detail.*
+import kotlinx.android.synthetic.main.content_anime_detail.*
 
 class AnimeDetailActivity : AppCompatActivity() {
 
     private lateinit var animeDetailViewModel: AnimeDetailViewModel
-    private lateinit var anime:AnimeDetail
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anime_detail)
-       // setSupportActionBar(toolbar)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
 
         animeDetailViewModel = ViewModelProviders.of(this).get(AnimeDetailViewModel::class.java)
 
-        val animeId = intent.getIntExtra(EXTRA_ANIME_ID,0) ?: return
+        val animeId = intent.getIntExtra(EXTRA_ANIME_ID,0)
 
         animeDetailViewModel.animesDetailLiveData.observe(this, Observer {
-            anime = it
-
-            val url = videoUrlToThumbUrl(anime.trailer_url!!)
-            loadImageView(iv_anime_video_thumb, url)
-
-            //Log.i(TAG_DEBUC, ""+it.title)
+            setData(it)
         })
+
+        iv_back.setOnClickListener{
+            finish()
+        }
 
         animeDetailViewModel.getAnime(animeId)
 
+    }
+
+    private fun setData(anime: AnimeDetail?) {
+        val url = videoUrlToThumbUrl(anime!!.trailer_url ?: "")
+        loadImageView(iv_anime_video_thumb, url)
+
+        tv_anime_detail_score.text = anime.score.toString()
+        tv_anime_title_detail.text = anime.title
+        tv_anime_detail_description.text = anime.synopsis
     }
 
     companion object {
