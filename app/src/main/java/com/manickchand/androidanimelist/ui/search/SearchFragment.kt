43034bccd03b989485f2219ad03040a25fe59915
Leon <lmj@icomp.ufam.edu.br>
2020-02-19 21:43:45 -0400

    (wip) search fragment

---
+++ app/src/main/java/com/manickchand/androidanimelist/ui/search/SearchFragment.kt
@@ -0,0 +1,19 @@
+package com.manickchand.androidanimelist.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.manickchand.androidanimelist.R
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+}
